9d76024293d045f7f48defe8837d9f77054bac7c
cintia.sestelo <sestelos@gmail.com>
2016-03-12 15:15:51 -0300

    My Django Girls app, first commit

--- Teste_elif_django.py
+++
@@ -1,9 +0,0 @@
-def hi(name):
-    if name == 'Ola':
-        print('Hey Ola!')
-    elif name == 'Cintia':
-        print('Hey Cintia!')
-    else:
-        print('Hey Anonymous!')
-
-hi('Sonja')
--- funcoes_dj.py
+++
@@ -1,5 +0,0 @@
-def hi():
-    print('Hi there!')
-    print('How are you?')
-
-hi()
